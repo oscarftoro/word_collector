61e6c76943f007bc82f0c70b67edfafe6f1f333d
oscar_felipe <groovenino@gmail.com>
2015-02-16 18:04:43 +0100

    supervise wc_backend and fix wc_mnesia.erl. Now words can be saved on mnesia

--- src/wc_mnesia.erl
+++ src/wc_mnesia.erl
@@ -9,22 +9,39 @@
 -module(wc_mnesia).
 -author("oscar").
 -include("../include/diccionario.hrl").
+
 %% API
--export([check_db_exist/0]).
+-export([check_db_exist/0, add_word/1]).
 
 setup_mnesia() ->
         mnesia:create_schema([node()]),
         mnesia:start(),
-        mnesia:create_table(word, [{attributes, record_info(fields, word)}]),
-        mnesia:create_table(language,[{attributes, record_info(fields, language)}]),
+        mnesia:create_table(word, [{type,set},{record_name, word},{attributes, record_info(fields, word)}]),
+        mnesia:create_table(language,[{type,set},{record_name,language},{attributes, record_info(fields, language)}]),
         mnesia:stop().
 
 
 
 check_db_exist()->
-
         case filelib:wildcard("Mnesia*") of
                 [Db_name] ->
                         io:fwrite("Database of name ~p detected ",[Db_name]);
                 [] -> setup_mnesia()
         end.
+
+
+
+-spec add_word(#word{}) -> {aborted,string()} | {atomic, string()}.
+
+add_word(W) ->
+
+        F = fun() ->
+                mnesia:write(W)
+        end,
+        case mnesia:transaction(F) of
+                {aborted,Reason} -> {aborted,Reason};
+                {atomic, Value} -> {atomic, Value}
+        end.
+
+
+
--- src/word_collector_sup.erl
+++ src/word_collector_sup.erl
@@ -8,5 +8,12 @@ start_link() ->
 	supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 init([]) ->
-	Procs = [],
-	{ok, {{one_for_one, 1, 5}, Procs}}.
+	%%Procs = [{wc_backend,{wc_backend,start_link(),[]},
+        %%permanent,1000,worker,[wc_backend,wc_mnesia]}],
+	{ok, {{one_for_one, 1, 5}, [child(wc_backend,none)]}}.
+
+child(Module, none) ->
+        {Module, {Module, start_link, []}, permanent, brutal_kill, worker, [Module]};
+child(Module, Name) ->
+        {{Module, Name}, {Module, start_link, [Name]},
+                permanent, brutal_kill, worker, [Module]}.
