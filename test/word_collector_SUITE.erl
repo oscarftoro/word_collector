81399de646e2f4bc1f86755c87b9763c5a126602
oscar_felipe <groovenino@gmail.com>
2015-03-01 19:18:27 +0100

    add more tests for the same previous functions

--- test/word_collector_SUITE.erl
+++ test/word_collector_SUITE.erl
@@ -2,14 +2,16 @@
 -author("oscar_toro").
 -include_lib("common_test/include/ct.hrl").
 -include("../include/diccionario.hrl").
--export([init_per_suite/1,all/0,add_word/1,
-	get_all_words/1,word_by_name/1]).
+-export([init_per_suite/1,init_per_testcase/2,end_per_testcase/2,
+	 all/0,add_word/1,
+	 get_all_words/1,word_by_name/1]).
 
 all() ->
     [add_word,get_all_words,word_by_name].
 
 init_per_suite(Config) ->
     Priv = ?config(priv_dir, Config),
+    %application:set_env(mnesia,dir,Priv),
     wc_mnesia:install([node()]),
     word_collector_app:start(),
     Config.
@@ -24,18 +26,27 @@ init_per_testcase(get_all_words,Config)->
     Config;
 init_per_testcase(word_by_name,Config) ->
     {atomic,ok} = wc_mnesia:add_word("blondine","rubia"),
-    {atomic,ok} = wc_mnesia:add_word("poloti", "policia"),
+    {atomic,ok} = wc_mnesia:add_word("politi", "policia"),
+    Config;
+init_per_testcase(_,Config) ->
     Config.
 
-
+end_per_testcase(_,_Config)->
+    ok.
 %% cases
 add_word(_Config) ->
     {atomic,ok} = wc_mnesia:add_word("kat","gato"),
     {atomic,ok} = wc_mnesia:add_word("robot","robot").
 
 get_all_words(_Config) ->
-   [One,Two] = wc_mnesia:get_all_words().
+   [One,Two,Three,Four] = wc_mnesia:get_all_words().
     
 word_by_name(_Config) ->
-    [Word] = wc_mnesia:find_word("kat"),
-    [] = wc_mnesia:find_word("køre").
+    [Word]  = wc_mnesia:find_word("kat"),
+    [Robot] = wc_mnesia:find_word("robot"),
+    [Rubia] = wc_mnesia:find_word("blondine"),
+    []      = wc_mnesia:find_word("køre"),
+    
+    Word#wc_word.title       =:= "kat",
+    Robot#wc_word.language   =:= "dk",
+    Rubia#wc_word.definition =:= "rubia".
