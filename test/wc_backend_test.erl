f63ac3d2ecfa317eac885e8c658048282b6ac7da
oscar_felipe <groovenino@gmail.com>
2015-03-07 14:45:59 +0100

    delete created words in wc_backend_test.erl

--- test/wc_backend_test.erl
+++ test/wc_backend_test.erl
@@ -3,26 +3,31 @@
 -include("../include/diccionario.hrl").
 -define(setup(F), {setup, fun start/0, fun stop/1, F}).
 
-generator_test() ->
+generator_add_find_word_test() ->
     {setup,
     fun start/0,
     fun stop/1,
-    fun add_word_test/1,
+    fun add_find_word_test/1,
     fun(Words) ->
     [Vin,Board] = Words,
     ?assertEqual("bord",Board#wc_word.title),
     ?assertEqual("vin",Vin#wc_word.title)
     end}.
+
 start() ->
     word_collector_app:start().
    
   
-stop(_)->
+stop(Words)->
+   [Vin,Board] = Words,
+   wc_mnesia:remove_word(Vin#wc_word.title),
+   wc_mnesia:remove_word(Board#wc_word.title),
    word_collector_app:stop().
 
-add_word_test(_) ->
+add_find_word_test(_) ->
     wc_backend:add_word("vin","vino"),
     wc_backend:add_word("bord","mesa"),
     [Vin]   = wc_mnesia:find_word("vin"),
     [Board] = wc_mnesia:find_word("bord"),
     [Vin,Board].
+
