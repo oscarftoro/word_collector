ca77f5930258d900adfd546fc77cecb8c4897c58
oscar_felipe <groovenino@gmail.com>
2015-03-15 18:58:40 +0100

    fix tests for wc_backend. Remove word_collector_SUITE since it is equivalent to wc_mnesia now.Ignore dialyzer plt table. Now we are ready to merge to master

--- test/wc_backend_SUITE.erl
+++ test/wc_backend_SUITE.erl
@@ -2,23 +2,50 @@
 -author("oscar_toro").
 -include_lib("common_test/include/ct.hrl").
 -include("../include/diccionario.hrl").
--export([all/0,groups/0,init_per_suite/1,end_per_suite/1,init_per_group/2,end_per_group/2]).
+-export([all/0,groups/0,init_per_suite/1,
+  end_per_suite/1,init_per_group/2,end_per_group/2,
+  init_per_testcase/2,end_per_testcase/2
+]).
 %% tests
--export([add_word_test/1,find_word_test/1]).
+-export([add_word_test/1,find_word_test/1,
+  get_all_words_test/1,delete_words_test/1]).
 all()->
-    [{group,addFind}].
+    [{group,addFind},{group,getAllDelete}].
 
 groups() ->
-  [{addFind,[sequence],[add_word_test,find_word_test]}].
+  [{addFind,[sequence],[add_word_test,find_word_test]},
+  {getAllDelete,[sequence],[get_all_words_test,delete_words_test]}].
 
 init_per_suite(Config) ->
-  application:load(sasl),
-  application:set_env(sasl,sasl_error_logger,false),
-  application:set_env(sasl,errlog_type,error),
-  error_logger:tty(false),
-  ok = application:start(sasl),
-  wc_mnesia:check_db_exist([node()]),
+  
+
+  wc_mnesia:install(),
   word_collector_app:start(),
+  Config.
+
+init_per_group(getAllDelete,Config)->
+  wc_backend:add_word("peter","pedro"),
+  wc_backend:add_word("mobiltelefon","celular"),
+  [{words,["peter","mobiltelefon"]}|Config];
+
+
+init_per_group(_,Config) ->
+  Config.
+
+init_per_testcase(_,Config)->
+  Config.
+end_per_testcase(get_all_words_test,_Config) ->
+  wc_mnesia:remove_word("uge");
+
+end_per_testcase(_,Config) ->
+  Config.
+
+end_per_group(addFind,Config)->
+  lists:foreach(fun(W) -> wc_mnesia:remove_word(W) end,["vin","bord"]),  
+  Config;
+
+end_per_group(getAllDelete,Config) ->
+
   Config.
 
 end_per_suite(_Config)->
@@ -26,16 +53,31 @@ end_per_suite(_Config)->
   application:stop(sasl),
   ok.
 
-init_per_group(_,Config) ->
-  Config.
-end_per_group(addFind,Config)->
-  lists:foreach(fun(W) -> wc_mnesia:remove_word(W) end,Config).
-
-add_word_test(_Config) ->
+add_word_test(Config) ->
   wc_backend:add_word("vin","vino"),
-  wc_backend:add_word("bord","mesa").
+  wc_backend:add_word("bord","mesa"),
+  Config.
 
-find_word_test(_Config) ->
-  [Vin]   = wc_mnesia:find_word("vin"),
-  [Board] = wc_mnesia:find_word("bord"),
-  [Vin,Board].
+find_word_test(Config) ->
+
+  [Vin]   = wc_backend:find_word("vin"),
+  [Board] = wc_backend:find_word("bord"),
+  "vin"   = Vin#wc_word.title,
+  "bord" = Board#wc_word.title,
+  Config.
+
+get_all_words_test(Config)->
+  Result = wc_backend:get_all_words(),
+  2 = length(Result),
+  wc_backend:add_word("uge", "semana"),
+  Result2 = wc_backend:get_all_words(),
+  3 = length(Result2),
+  Config.
+%todo: check that available is false
+delete_words_test(_Config)->
+  wc_backend:delete_word("peter"),
+  1 = length(wc_backend:get_all_words()),
+  wc_backend:delete_word("mobiltelefon"),
+  0 = length(wc_backend:get_all_words()).
+ 
+    
---
+++ test/wc_mnesia_SUITE.erl
@@ -0,0 +1,67 @@
+-module(wc_mnesia_SUITE).
+-author("oscar_toro").
+-include_lib("common_test/include/ct.hrl").
+-include("../include/diccionario.hrl").
+-export([init_per_suite/1,end_per_suite/1,init_per_testcase/2,
+  end_per_testcase/2,  all/0,add_word/1,
+  get_all_words/1,get_all_words2/1,find_word_by_name/1, 
+  edit_word/1,delete_word/1]).
+
+all() ->  
+  [add_word,get_all_words,find_word_by_name,
+  get_all_words2,edit_word,delete_word].
+
+init_per_suite(Config) ->
+  word_collector_app:start(),
+  Config.
+
+end_per_suite(_Config) ->
+  application:stop(mnesia),
+  ok.
+
+init_per_testcase(find_word_by_name,Config) ->
+  {atomic,ok} = wc_mnesia:add_word("blondine","rubia"),
+  {atomic,ok} = wc_mnesia:add_word("politi", "policia"),
+  Config;
+
+init_per_testcase(_,Config)->
+  Config.
+
+end_per_testcase(_Rest,_Config)->
+  ok.
+%% cases
+add_word(_Config) ->
+  {atomic,ok} = wc_mnesia:add_word("kat","gato"),
+  {atomic,ok} = wc_mnesia:add_word("robot","robot").
+
+get_all_words(_Config) ->
+  Result = wc_mnesia:get_all_words(),
+  2 = length(Result).
+  
+    
+find_word_by_name(_Config) ->
+  [Word]   = wc_mnesia:find_word("kat"),
+  [Robot]  = wc_mnesia:find_word("robot"),
+  [Rubia]  = wc_mnesia:find_word("blondine"),
+  []       = wc_mnesia:find_word("køre"),
+  [Politi] = wc_mnesia:find_word("politi"),
+    
+  "kat"     = Word#wc_word.title,
+  "dk"      =  Robot#wc_word.language,
+  "rubia"   = Rubia#wc_word.definition,
+  "policia" = Politi#wc_word.definition.
+get_all_words2(_Config) ->    
+  Result = wc_mnesia:get_all_words(),
+  4 = length(Result).
+
+edit_word(_Config) ->
+  {atomic,ok}   = wc_mnesia:edit_word("kat",definition,"gato o gata"),
+  [Result]      = wc_mnesia:find_word("kat"),
+  "gato o gata" = Result#wc_word.definition.
+  
+delete_word(_Config) ->
+  {atomic,ok} = wc_mnesia:delete_word("kat"),
+  [Kat]       = wc_mnesia:find_word("kat"),% the word is found because as facebook, we do not delete things! we just disable them.
+  false       = Kat#wc_word.available.
+  
+
