963e866735a8fd8c6f1bef3014e7de6c8c92b552
oscar_felipe <groovenino@gmail.com>
2015-04-24 02:42:44 +0200

    implement json encoding in wc_json.erl

--- src/wc_json.erl
+++ src/wc_json.erl
@@ -1,18 +1,45 @@
 -module(wc_json).
--export([json_to_record/1]).
+-export([encode/1,decode/1,record_to_proplist/1]).
+
+
 -include_lib("eunit/include/eunit.hrl").
 -include("../include/diccionario.hrl").
--include_lib("../deps/jsonrec/include/jsonrec.hrl").
 
-json_to_record(Json)->
-  {ok,Record} = word:from_json(Json),
-  Record.
+-spec encode(#wc_word{}) -> binary().
+encode(#wc_word{} = Rec) ->  
+  jiffy:encode({record_to_proplist(Rec)});
+encode(#wc_language{} = Rec) ->
+  jiffy:encode({record_to_proplist(Rec)}).
 
-json_to_record_test()->
-  W = #wc_word{title="numse", definition="popin"},
-  W = json_to_record(<<"{\"word\":{\"title\": \"numse\",
-  \"definition\":\"popin\"}}">>).
+decode(Bin) ->
+    jiffy:decode(Bin).
 
-record_to_json_test() ->
-    W = #wc_word{title="numse", definition="popin"},
+-spec record_to_proplist(#wc_word{}) -> [{atom(),any()}].
+record_to_proplist(#wc_word{} = Rec) ->
+  lists:zip(record_info(fields,wc_word),   
+  tl(tuple_to_list(Rec)));
+
+record_to_proplist(#wc_language{} = Rec) ->
+  lists:zip(record_info(fields,wc_language),
+  tl(tuple_to_list(Rec))).
+
+
+encode_test() ->
+
+  W = #wc_word{title = <<"numse">>, definition = <<"popin">>},
+  Encoded = encode(W),
+  W2 = decode(Encoded),
     
+  ?assert(<<"numse">> =:= W2#wc_word.title),     
+  ?assert(<<"popin">> =:= W2#wc_word.definition).
+    
+
+ 
+%% decode_test()->
+%%   W = #wc_word{title="numse", definition="popin"},
+%%   ok = decode(<<"{\"word\":{\"title\": \"numse\",
+%%   \"definition\":\"popin\"}}">>),
+%%    W, true =:= true.
+
+
+
