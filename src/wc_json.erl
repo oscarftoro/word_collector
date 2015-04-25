507b1538fa16097ea61077b55d18f28d0fbb6065
oscar_felipe <groovenino@gmail.com>
2015-04-25 02:02:43 +0200

    implement encoding from words and language records to json and implement decoder of wc_word records

--- src/wc_json.erl
+++ src/wc_json.erl
@@ -11,8 +11,16 @@ encode(#wc_word{} = Rec) ->
 encode(#wc_language{} = Rec) ->
   jiffy:encode({record_to_proplist(Rec)}).
 
+%% TODO: Implement languages also
+%% decode only words
 decode(Bin) ->
-    jiffy:decode(Bin).
+  {PrpList} = jiffy:decode(Bin),
+  PropList  = [{binary_to_atom(K,utf8),V} || {K,V} <- PrpList],
+  Seq       = lists:seq(2,11),
+  Values    = [V ||  {_K,V} <- PropList], %% all the values
+  Formated  = lists:zip(Seq,Values), %proplist{int,binary() | []}
+  Word      = #wc_word{},
+  lists:foldr(fun({K,V},Acc) -> setelement(K,Acc,V) end,Word,Formated).
 
 -spec record_to_proplist(#wc_word{}) -> [{atom(),any()}].
 record_to_proplist(#wc_word{} = Rec) ->
@@ -23,13 +31,11 @@ record_to_proplist(#wc_language{} = Rec) ->
   lists:zip(record_info(fields,wc_language),
   tl(tuple_to_list(Rec))).
 
-
 encode_test() ->
 
   W = #wc_word{title = <<"numse">>, definition = <<"popin">>},
   Encoded = encode(W),
   W2 = decode(Encoded),
-    
   ?assert(<<"numse">> =:= W2#wc_word.title),     
   ?assert(<<"popin">> =:= W2#wc_word.definition).
     
