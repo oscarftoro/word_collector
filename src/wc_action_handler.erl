32500ac19fa62a68e20c05bdb210a49aebf8830c
oscar_felipe <groovenino@gmail.com>
2015-05-04 22:52:38 +0200

    get the query list of values from POST and PUT

--- src/wc_action_handler.erl
+++ src/wc_action_handler.erl
@@ -12,11 +12,12 @@
 %% API
 -export([init/3]).
 -export([content_types_provided/2,
-	allowed_methods/2]).
+	 allowed_methods/2,
+         content_types_accepted/2]).
 -export([handle_request/2]).
+-export([create_resource/2]).
 -include("../include/diccionario.hrl").
-%% define a debug macro
-%% c(wc_action_handler,{d,debug_flag}).
+
 -ifdef(debug_flag).
 -define(DEBUG(X),io:format("DEBUG ~p: ~p ~p~n",[?MODULE,?LINE,X])).
 -else.
@@ -33,28 +34,43 @@ allowed_methods(Req,State)->
 content_types_provided(Req, State) ->
   {[{<<"application/json">>, handle_request}], Req, State}.
 
-handle_request(Req,State)->
+content_types_accepted(Req, State) ->
+  {[{{<<"application">>, <<"x-www-form-urlencoded">>, []}, create_resource}],Req, State}.
+
+handle_request(Req,State) ->
   handle_method(cowboy_req:get(method,Req),Req,State).
+
+create_resource(Req,State) ->
+  create_or_edit(cowboy_req:get(method,Req),Req,State).
 
 -spec handle_method(<<_:24,_:_*8>>,_,_)->{<<_:64,_:_*8>>,_,_}.
 handle_method(<<"GET">>,Req,State)->
   {AllBindings,_Req1} = cowboy_req:bindings(Req),
   ?DEBUG(AllBindings),
    
-  Words = get_resources(AllBindings),
+  Words = get_resource(AllBindings),
   ?DEBUG(Words),
-  {Words,Req,State};
-%to create a new Word
-handle_method(<<"PUT">>,Req,State) ->
+  {Words,Req,State}.
+
+%% To create a new Word
+%%
+create_or_edit(<<"PUT">>,Req,State) ->
   {AllBindings,_Req1} = cowboy_req:bindings(Req),
   ?DEBUG(AllBindings),
+  {AllValues,Req2}   = cowboy_req:qs_vals(Req),
+  ?DEBUG(AllValues),
+  ?DEBUG(Req2),
   Result = handle_put(AllBindings,Req),
   {Result,Req,State};
 
-handle_method(<<"DELETE">>,Req,State)->
-  {<<"That was a DELETE method">>,Req,State}.
+create_or_edit(<<"POST">>,Req,State) ->
+   {AllBindings,_Req1} = cowboy_req:bindings(Req),
+   ?DEBUG(AllBindings),  
+   {AllValues,_Req2}   = cowboy_req:qs_vals(Req),
+   ?DEBUG(AllValues),
+   {<<"That was a POST">>,Req,State}.
 
-%% when creating resource is a word
+    
 handle_put([{first,What}],Req) when What =:= <<"word">> ->
   {Word_title,_Req} = cowboy_req:qs_val(<<"title">>,Req), 
   ?DEBUG(Word_title),
@@ -67,19 +83,19 @@ handle_put([{first,What}],Req) when What =:= <<"word">> ->
 %%%REST RESOURCES%%%
 %%%%%%%%%%%%%%%%%%%%
               
-get_resources([{what,<<"words">>}]) ->
+get_resource([{what,<<"words">>}]) ->
   Words = wc_backend:get_all_words(),
-  ?DEBUG(Words),
+
   Result = wc_json:encode(Words),
-  ?DEBUG(Result),Result;
+  Result;
  
-get_resources([what,<<"languages">>]) ->
+get_resource([what,<<"languages">>]) ->
     ok;
 
-get_resources([{item,ItemName},{what,<<"words">>}]) ->
+get_resource([{item,ItemName},{what,<<"words">>}]) ->
   ItemName,ok;
 		  
-get_resources([{item,ItemName},{what,<<"language">>}])->
+get_resource([{item,ItemName},{what,<<"language">>}])->
   ItemName,ok.		     
 		  
     
